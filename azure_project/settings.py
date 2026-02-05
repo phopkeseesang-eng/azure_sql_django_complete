22144ba0125d8842a5f153d5acb6f73448615f47
phopkeseesang-eng <phop.keseesang@mail.kmutt.ac.th>
2026-02-05 15:16:07 +0700

    Update settings.py

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -81,25 +81,25 @@ WSGI_APPLICATION = 'azure_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+ DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+ }
 DATABASES = {
-    'default': {
-        'ENGINE': 'mssql',
-        'NAME': 'bearlab-sql-db',
-        'USER': 'bearlab-admin',
-        'PASSWORD': 'P@ss1234',
-        'HOST': 'bearlab-sql-server.database.windows.net',
-        'PORT': '1433',
-        'OPTIONS': {
-            'driver': 'ODBC Driver 18 for SQL Server',
-        },
-    },
-}
+     'default': {
+         'ENGINE': 'mssql',
+         'NAME': 'free-sql-db-4467991',
+         'USER': 'phop-admin',
+         'PASSWORD': 'Tatsumaki-Yuya12',
+         'HOST': 'phop-sql-server.database.windows.net',
+         'PORT': '1433',
+         'OPTIONS': {
+             'driver': 'ODBC Driver 18 for SQL Server',
+         },
+     },
+ }
 
 
 import urllib.parse
