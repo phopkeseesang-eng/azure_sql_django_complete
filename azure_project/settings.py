23886d0cf4649f4dad702037e6813af0b69d2ba7
phopkeseesang-eng <phop.keseesang@mail.kmutt.ac.th>
2026-02-05 17:26:28 +0700

    Update settings.py

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -106,8 +106,8 @@ DATABASES = {
 import urllib.parse
 
 # MongoDB Configuration
-mongo_username = urllib.parse.quote_plus('bearlab')
-mongo_password = urllib.parse.quote_plus('P@ss1234')
+mongo_username = urllib.parse.quote_plus('phop2547')
+mongo_password = urllib.parse.quote_plus('Tatsumaki-Yuya12')
 # MONGO_URI = 'mongodb://localhost:27017/'
 MONGO_URI = f'mongodb+srv://{mongo_username}:{mongo_password}@bearlab-mongodb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
 MONGO_DB_NAME = 'django_store_reviews'
