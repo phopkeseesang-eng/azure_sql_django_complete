056010fa8cc32b0fe5c5af3f8be713f27b22ae59
phopkeseesang-eng <phop.keseesang@mail.kmutt.ac.th>
2026-02-05 20:47:09 +0700

    Update settings.py

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -109,9 +109,9 @@ import urllib.parse
 mongo_username = urllib.parse.quote_plus('phop2547')
 mongo_password = urllib.parse.quote_plus('Tatsumaki-Yuya12')
 # MONGO_URI = 'mongodb://localhost:27017/'
-MONGO_URI = f'mongodb+srv://{mongo_username}:{mongo_password}@bearlab-mongodb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
-MONGO_DB_NAME = 'django_store_reviews'
-    
+MONGO_URI = f'mongodb+srv://{mongo_username}:{mongo_password}@phopdb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
+MONGO_DB_NAME = 'phopdb'
+
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
